fffa49b80910f5ef9f356d47ac58266ed9a9a7ab
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-07-09 11:57:35 +0200

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -3,5 +3,5 @@ from .synth import SynthDetection
 from .td500 import TD500Detection
 from .coco import COCODetection
 from .mlt import MLTDetection
-from .utils import AnnotationTransform, detection_collate
+from .utils import AnnotationTransform, detection_collate, get_boxes
 from .config import *
