f3e74302b93a1148c7a6461d1148613ebca9ab2f
soldierofhell <45559276+soldierofhell@users.noreply.github.com>
2019-07-23 15:27:41 +0200

    Update multibox_loss.py

--- layers/modules/multibox_loss.py
+++ layers/modules/multibox_loss.py
@@ -16,12 +16,13 @@ class MultiBoxLoss(nn.Module):
 
     def forward(self, predictions, targets, segs):
         loc_data, conf_data, priors, seg_data = predictions
-        # loc_data - offset branch, [batch_size=8, \sum_k w_kxh_kxk=120272, q=4, offsets=4]
-        # conf_data - score branch, [batch_size=8, \sum_k w_kxh_kxk=120272, q=4, scores=2]) 
+        # loc_data - offset branch, [batch_size, \sum_k w_kxh_kxk=120272, q=4, offsets=4]
+        # conf_data - score branch, [batch_size, \sum_k w_kxh_kxk=120272, q=4, scores=2]) 
         # priors - default boxes [\sum_k w_kxh_kxk=120272, x_1,x_2,s_x1,s_x2=4]
         # seg_data - [8388608, 1]
         
         num = loc_data.size(0)
+        print("num: ", num)
         priors = priors[:loc_data.size(1), :]
         num_priors = priors.size(0)
         num_classes = self.num_classes
@@ -45,14 +46,18 @@ class MultiBoxLoss(nn.Module):
         # wrap targets
         loc_t = Variable(loc_t, requires_grad=False)
         conf_t = Variable(conf_t, requires_grad=False)
+        # conf_t: [batch_size, \sum_k w_kxh_kxk=120272, q=4]
 
         pos = conf_t > 0
+        # pos: [batch_size, \sum_k w_kxh_kxk=120272, q=4]
         
         print('pos shape: ', pos.size())
         
         # Localization Loss (Smooth L1)
         # Shape: [batch,num_priors,4]
         pos_idx = pos.unsqueeze(pos.dim()).expand_as(loc_data)
+        # pos_idx, [8, 120272, 4, 4]
+        
         print(pos_idx.size())
         
         loc_p = loc_data[pos_idx].view(-1, 4)
@@ -62,13 +67,14 @@ class MultiBoxLoss(nn.Module):
         # Compute max conf across batch for hard negative mining
         batch_conf = conf_data.view(-1, self.num_classes)
         print(batch_conf.size())
+        # batch_conf: [batch_size * (\sum_k w_k * h_k * k) * q, 2]
 
         loss_c = log_sum_exp(batch_conf) - batch_conf.gather(1, conf_t.view(-1, 1))
         
         # loss_c, [3848704, 1]
         
         # Hard Negative Mining
-        loss_c[pos] = 0  # filter out pos boxes for now
+        loss_c[pos.view(-1,1)] = 0  # filter out pos boxes for now
         loss_c = loss_c.view(num, -1)
         _, loss_idx = loss_c.sort(1, descending=True)
         _, idx_rank = loss_idx.sort(1)
